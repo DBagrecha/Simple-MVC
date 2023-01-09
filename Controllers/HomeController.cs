2ec5c9a3a944c7e855772d8d273f4c77f9795ac9
HELLO <HELLO@DESKTOP-B116MUL>
2023-01-09 19:36:07 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+using System.Diagnostics;
+using System.Web;
+using MongoDB.Driver;
+using System.Net;
+
+namespace WebApplication1.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        MongoClient client = new MongoClient("mongodb://localhost:27017/");
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public string getIP()
+        {
+            string ip = Response.HttpContext.Connection.RemoteIpAddress.ToString();
+            return ip;
+        }
+    
+        public IActionResult Index()
+        {
+            var database = client.GetDatabase("demo");
+            var tab = database.GetCollection<Table>("data");
+            Table table1 = new Table();
+            table1.date = DateTime.Now.ToString();
+            table1.ip = getIP();
+            tab.InsertOne(table1);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            ViewData["IP"] = getIP();
+            ViewData["Date"] = DateTime.Now;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        
+    }
+}
---
+++ Models/Table.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class Table
+    {
+        public string ip { get; set; }
+        public string date { get; set; }
+    }
+}
